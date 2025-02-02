71652b2fda9619ede58722242d3b3e4eeccbd336
Touafic BOUAB <eclairetoilegentlemans@gmail.com>
2025-02-02 02:43:18 -0500

    write payment model

--- payment_info_gen/bill/models.py
+++ payment_info_gen/bill/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Payment(models.Model):
+    payment_id = models.CharField(max_length=255)
+    payment_method = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    payment_date = models.DateTimeField()
+    payment_time = models.TimeField()
+
+    def __str__(self):
+        # return f'{self.payment_id} - {self.user.last_name}'
+        return f'{self.payment_id}'
